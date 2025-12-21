c3159c25b2d088635794fa6e8d22a0afe9eb25b9
akcabra <andraz.klobucar@gmail.com>
2025-12-21 15:26:37 +0100

    HIKU-15: Dodani osnovni java classi in config

---
+++ src/main/java/com/hiku/notificationService/NotificationService.java
@@ -0,0 +1,8 @@
+package com.hiku.notificationService;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/notifications")
+public class NotificationService extends Application {
+}
